874dcec19cb408c1f58b6d0d162d2c7ad5158b4f
ElaineMolina <elainemolina10@gmail.com>
2021-11-29 10:56:22 -0300

    Add método listarTodos da classe PessoaDAO

--- src/br/com/molina/dao/PessoaDAO.java
+++ src/br/com/molina/dao/PessoaDAO.java
@@ -2,10 +2,11 @@ package br.com.molina.dao;
 
 import br.com.molina.dto.PessoaDTO;
 import br.com.molina.jdbc.ConexaoUtil;
-
-import java.awt.image.ShortLookupTable;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class PessoaDAO {
@@ -39,4 +40,27 @@ public class PessoaDAO {
             e.printStackTrace();
         }
     }
+
+    public List<PessoaDTO> listarTodos(){
+        List<PessoaDTO> listaPessoas = new ArrayList<PessoaDTO>();
+        try{
+            Connection connection = ConexaoUtil.getInstance().getConnection();
+            String sql = "SELECT * FROM PESSOA";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet resultset = statement.executeQuery();
+            while(resultset.next()){
+                PessoaDTO pessoaDTO = new PessoaDTO();
+                pessoaDTO.setId_pessoa(resultset.getInt("ID_PESSOA"));
+                pessoaDTO.setNome(resultset.getString("NOME"));
+
+                listaPessoas.add(pessoaDTO);
+            }
+            connection.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return listaPessoas;
+    }
+
+
 }
