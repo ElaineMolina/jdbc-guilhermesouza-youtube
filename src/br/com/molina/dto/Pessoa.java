a77560581bf1b9d2279c1e9d99ac76c519fde133
ElaineMolina <elainemolina10@gmail.com>
2021-11-29 10:12:37 -0300

    Pacote DTO classe Pessoa

---
+++ src/br/com/molina/dto/Pessoa.java
@@ -0,0 +1,22 @@
+package br.com.molina.dto;
+
+public class Pessoa {
+    private int id_pessoa;
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId_pessoa() {
+        return id_pessoa;
+    }
+
+    public void setId_pessoa(int id_pessoa) {
+        this.id_pessoa = id_pessoa;
+    }
+}
